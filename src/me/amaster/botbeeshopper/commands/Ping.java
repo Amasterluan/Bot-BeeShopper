d8fc05ac1f95e6ba3a209d6945588b8189950c6f
Luan Moreira <amasterluan@gmail.com>
2023-01-11 23:11:47 -0300

    Versão 0.0.2

---
+++ src/me/amaster/botbeeshopper/commands/Ping.java
@@ -0,0 +1,25 @@
+package me.amaster.botbeeshopper.commands;
+
+import me.amaster.botbeeshopper.main.BeeShopper;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Ping extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+
+        String[] args = event.getMessage().getContentRaw().split(" ");
+        TextChannel textChannel = event.getChannel().asTextChannel();
+
+        if(args[0].equalsIgnoreCase("!" + "ping")){
+
+            textChannel.sendMessage(BeeShopper.jda.getGatewayPing()+ "ms").queue();
+
+
+
+        }
+
+    }
+}
